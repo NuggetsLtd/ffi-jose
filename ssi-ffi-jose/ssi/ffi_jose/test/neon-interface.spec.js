56078ca1019fbcfa878a17c1b3e7c48fce5ea0f9
Andrew Lord <andrew.lord@appliedblockchain.com>
2022-07-05 12:36:35 +0100

    :white_check_mark: (ffi_jose) Tests for ciphertext decryption

--- ssi-ffi-jose/ssi/ffi_jose/test/neon-interface.spec.js
+++ ssi-ffi-jose/ssi/ffi_jose/test/neon-interface.spec.js
@@ -139,7 +139,7 @@ describe('NEON NodeJS Interface:', () => {
         describe('where enc type is `GCM`', () => {
           const iv = Uint8Array.from(Buffer.from('eae7e2df51f0dc34c39183e8', 'hex')).buffer
 
-          it('where enc=`A128GCM`', () => {
+          it('and enc=`A128GCM`', () => {
             const enc = ContentEncryption.A128GCM
             const key = Uint8Array.from(Buffer.from('b8aae648b9c7819e24f2b2c684efcef1', 'hex')).buffer
 
@@ -149,7 +149,7 @@ describe('NEON NodeJS Interface:', () => {
             expect(encrypted.tag).toBe('aFebG2ev+cSucVzhgvnePw==')
           })
 
-          it('where enc=`A192GCM`', () => {
+          it('and enc=`A192GCM`', () => {
             const enc = ContentEncryption.A192GCM
             const key = Uint8Array.from(Buffer.from('5d9e61b7536901f89ffe729b2e94917987d6aee671d7c1a7', 'hex')).buffer
 
@@ -159,7 +159,7 @@ describe('NEON NodeJS Interface:', () => {
             expect(encrypted.tag).toBe('fXf8J7mEhy3Eqxz1mnkwQA==')
           })
 
-          it('where enc=`A256GCM`', () => {
+          it('and enc=`A256GCM`', () => {
             const enc = ContentEncryption.A256GCM
             const key = Uint8Array.from(Buffer.from('4f0579c975d04ae004c9a2fd6620ad10bf763159a0e6894c6c0818acc5c24854', 'hex')).buffer
 
@@ -174,7 +174,7 @@ describe('NEON NodeJS Interface:', () => {
         describe('where enc type is `CBC`', () => {
           const iv = Uint8Array.from(Buffer.from('5ee779854f0e37e83f39441c86cebe90', 'hex')).buffer
 
-          it('where enc=`A128CBC-HS256`', () => {
+          it('and enc=`A128CBC-HS256`', () => {
             const enc = ContentEncryption['A128CBC-HS256']
             const key = Uint8Array.from(Buffer.from('4f0579c975d04ae004c9a2fd6620ad10bf763159a0e6894c6c0818acc5c24854', 'hex')).buffer
 
@@ -184,7 +184,7 @@ describe('NEON NodeJS Interface:', () => {
             expect(encrypted.tag).toBe('8BR2fw5Twj9f7/5S7BEEhw==')
           })
 
-          it('where enc=`A192CBC-HS384`', () => {
+          it('and enc=`A192CBC-HS384`', () => {
             const enc = ContentEncryption['A192CBC-HS384']
             const key = Uint8Array.from(Buffer.from('1d859097f5c1c883bdb5947466a85c2182373e94087b6f9895bc082e476da8d29817b0966db6e8003706d4d4daaf5a86', 'hex')).buffer
 
@@ -194,7 +194,7 @@ describe('NEON NodeJS Interface:', () => {
             expect(encrypted.tag).toBe('vylJiL27sZ8Sq63KXNfslNb/aqRhRSyk')
           })
 
-          it('where enc=`A256CBC-HS512`', () => {
+          it('and enc=`A256CBC-HS512`', () => {
             const enc = ContentEncryption['A256CBC-HS512']
             const key = Uint8Array.from(Buffer.from('cbd2a7b6f333ace24f3b7dad6579b40f97546ea59b3cf2325100ab78e46126d0521e515aa33e2af140308988d06ea15f96a0d3c794b311a755dca5ace7fa1e94', 'hex')).buffer
 
@@ -202,6 +202,87 @@ describe('NEON NodeJS Interface:', () => {
 
             expect(encrypted.ciphertext).toBe('4CN6Qy2JV/+MeALSoHcKDg==')
             expect(encrypted.tag).toBe('ZerEUmGJgIblArz+GrdTmP0BNzk2fgqm71OgS6GzzJs=')
+          })
+
+        })
+
+      })
+
+    })
+
+    describe.only('decrypt()', () => {
+      const plaintextb64 = 'UExBSU5URVhU'
+      const plaintext = base64ToArrayBuffer(plaintextb64)
+      const aad = base64ToArrayBuffer('')
+
+      describe('should decrypt correctly', () => {
+
+        describe('where enc type is `GCM`', () => {
+          const iv = Uint8Array.from(Buffer.from('eae7e2df51f0dc34c39183e8', 'hex')).buffer
+
+          it('and enc=`A128GCM`', () => {
+            const enc = ContentEncryption.A128GCM
+            const key = Uint8Array.from(Buffer.from('b8aae648b9c7819e24f2b2c684efcef1', 'hex')).buffer
+            const encrypted = jose.encrypt(enc, plaintext, key, iv, aad)
+
+            const message = jose.decrypt(enc, key, base64ToArrayBuffer(encrypted.ciphertext), iv, base64ToArrayBuffer(encrypted.tag), aad)
+
+            expect(message).toBe(plaintextb64)
+          })
+
+          it('and enc=`A192GCM`', () => {
+            const enc = ContentEncryption.A192GCM
+            const key = Uint8Array.from(Buffer.from('5d9e61b7536901f89ffe729b2e94917987d6aee671d7c1a7', 'hex')).buffer
+            const encrypted = jose.encrypt(enc, plaintext, key, iv, aad)
+
+            const message = jose.decrypt(enc, key, base64ToArrayBuffer(encrypted.ciphertext), iv, base64ToArrayBuffer(encrypted.tag), aad)
+
+            expect(message).toBe(plaintextb64)
+          })
+
+          it('and enc=`A256GCM`', () => {
+            const enc = ContentEncryption.A256GCM
+            const key = Uint8Array.from(Buffer.from('4f0579c975d04ae004c9a2fd6620ad10bf763159a0e6894c6c0818acc5c24854', 'hex')).buffer
+            const encrypted = jose.encrypt(enc, plaintext, key, iv, aad)
+
+            const message = jose.decrypt(enc, key, base64ToArrayBuffer(encrypted.ciphertext), iv, base64ToArrayBuffer(encrypted.tag), aad)
+
+            expect(message).toBe(plaintextb64)
+          })
+
+        })
+
+        describe('where enc type is `CBC`', () => {
+          const iv = Uint8Array.from(Buffer.from('5ee779854f0e37e83f39441c86cebe90', 'hex')).buffer
+
+          it('and enc=`A128CBC-HS256`', () => {
+            const enc = ContentEncryption['A128CBC-HS256']
+            const key = Uint8Array.from(Buffer.from('4f0579c975d04ae004c9a2fd6620ad10bf763159a0e6894c6c0818acc5c24854', 'hex')).buffer
+            const encrypted = jose.encrypt(enc, plaintext, key, iv, aad)
+
+            const message = jose.decrypt(enc, key, base64ToArrayBuffer(encrypted.ciphertext), iv, base64ToArrayBuffer(encrypted.tag), aad)
+
+            expect(message).toBe(plaintextb64)
+          })
+
+          it('and enc=`A192CBC-HS384`', () => {
+            const enc = ContentEncryption['A192CBC-HS384']
+            const key = Uint8Array.from(Buffer.from('1d859097f5c1c883bdb5947466a85c2182373e94087b6f9895bc082e476da8d29817b0966db6e8003706d4d4daaf5a86', 'hex')).buffer
+            const encrypted = jose.encrypt(enc, plaintext, key, iv, aad)
+
+            const message = jose.decrypt(enc, key, base64ToArrayBuffer(encrypted.ciphertext), iv, base64ToArrayBuffer(encrypted.tag), aad)
+
+            expect(message).toBe(plaintextb64)
+          })
+
+          it('and enc=`A256CBC-HS512`', () => {
+            const enc = ContentEncryption['A256CBC-HS512']
+            const key = Uint8Array.from(Buffer.from('cbd2a7b6f333ace24f3b7dad6579b40f97546ea59b3cf2325100ab78e46126d0521e515aa33e2af140308988d06ea15f96a0d3c794b311a755dca5ace7fa1e94', 'hex')).buffer
+            const encrypted = jose.encrypt(enc, plaintext, key, iv, aad)
+
+            const message = jose.decrypt(enc, key, base64ToArrayBuffer(encrypted.ciphertext), iv, base64ToArrayBuffer(encrypted.tag), aad)
+
+            expect(message).toBe(plaintextb64)
           })
 
         })
