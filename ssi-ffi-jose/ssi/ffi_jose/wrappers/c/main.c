52148ef7d5f1f98ff6538158877ab1a5b80428f6
Andrew Lord <andrew.lord@appliedblockchain.com>
2022-07-13 23:20:56 +0100

    :white_check_mark: (ffi_jose) Add tests for JSON encrypt & decrypt in C

--- ssi-ffi-jose/ssi/ffi_jose/wrappers/c/main.c
+++ ssi-ffi-jose/ssi/ffi_jose/wrappers/c/main.c
@@ -20,7 +20,7 @@ void string2ByteArray(char* input, BYTE* output)
 
 void generateKeyPairJWK(NamedCurve named_curve)
 {
-  JwkJsonString json_string;
+  JsonString json_string;
 
   int outcome = ffi_jose_generate_key_pair_jwk(named_curve, &json_string);
 
@@ -29,12 +29,12 @@ void generateKeyPairJWK(NamedCurve named_curve)
     printf("Generated JWK:\n%s\n\n", json_string.ptr);
   }
 
-  ffi_jose_free_jwk_string(json_string);
+  ffi_jose_free_json_string(json_string);
 }
 
 void encryptMsg(ContentEncryptionAlgorithm enc, char* key, char* iv, char* plaintext)
 {
-  EncryptedJsonString json_string;
+  JsonString json_string;
   ByteArray keyBuffer;
   ByteArray ivBuffer;
   ByteArray messageBuffer;
@@ -76,12 +76,12 @@ void encryptMsg(ContentEncryptionAlgorithm enc, char* key, char* iv, char* plain
     printf("Encrypted Msg:\n%s\n\n", json_string.ptr);
   }
 
-  ffi_jose_free_encrypted_string(json_string);
+  ffi_jose_free_json_string(json_string);
 }
 
 void decryptMsg(ContentEncryptionAlgorithm enc, char* key, char* ciphertext, char* iv, char* tag)
 {
-  DecryptedString json_string;
+  JsonString json_string;
   ByteArray keyBuffer;
   ByteArray ciphertextBuffer;
   ByteArray ivBuffer;
@@ -138,7 +138,63 @@ void decryptMsg(ContentEncryptionAlgorithm enc, char* key, char* ciphertext, cha
     printf("Decrypted Msg (Base64):\n%s\n\n", json_string.ptr);
   }
 
-  ffi_jose_free_decrypted_string(json_string);
+  ffi_jose_free_json_string(json_string);
+}
+
+void generalEncryptJson(KeyEncryptionAlgorithm alg, ContentEncryptionAlgorithm enc, char* recipients, char* payload)
+{
+  JsonString json_string;
+  ByteArray recipientsBuffer;
+  ByteArray payloadBuffer;
+
+  // populate recipients buffer
+  recipientsBuffer.length = strlen(recipients);
+  BYTE recipientsBufferData[recipientsBuffer.length];
+  string2ByteArray(recipients, recipientsBufferData);
+  recipientsBuffer.data = recipientsBufferData;
+
+  // populate payload buffer
+  payloadBuffer.length = strlen(payload);
+  BYTE payloadBufferData[payloadBuffer.length];
+  string2ByteArray(payload, payloadBufferData);
+  payloadBuffer.data = payloadBufferData;
+
+  int outcome = ffi_jose_general_encrypt_json(alg, enc, payloadBuffer, recipientsBuffer, &json_string);
+
+  if (outcome == 0)
+  {
+    printf("General Encrypted Msg:\n%s\n\n", json_string.ptr);
+  }
+
+  ffi_jose_free_json_string(json_string);
+}
+
+void decryptJson(char* jwe, char* jwk)
+{
+  JsonString json_string;
+  ByteArray jweBuffer;
+  ByteArray jwkBuffer;
+
+  // populate jwe buffer
+  jweBuffer.length = strlen(jwe);
+  BYTE jweBufferData[jweBuffer.length];
+  string2ByteArray(jwe, jweBufferData);
+  jweBuffer.data = jweBufferData;
+
+  // populate jwk buffer
+  jwkBuffer.length = strlen(jwk);
+  BYTE jwkBufferData[jwkBuffer.length];
+  string2ByteArray(jwk, jwkBufferData);
+  jwkBuffer.data = jwkBufferData;
+
+  int outcome = ffi_jose_decrypt_json(jweBuffer, jwkBuffer, &json_string);
+
+  if (outcome == 0)
+  {
+    printf("Decrypted Msg:\n%s\n\n", json_string.ptr);
+  }
+
+  ffi_jose_free_json_string(json_string);
 }
 
 int main()
@@ -154,10 +210,24 @@ int main()
 
   char* key = "b8aae648b9c7819e24f2b2c684efcef1";
   char* iv = "eae7e2df51f0dc34c39183e8";
-  char* plaintext = "MESSAGE_TO_ENCRYPT";
+  char* plaintext = "PLAINTEXT";
   encryptMsg(A128gcm, key, iv, plaintext);
 
   char* ciphertext = "862dc0141058b5ca4768f7928a3c93fa35c6";
   char* tag = "aba4fd9f3ab9dde676f7b1a91f562b35";
   decryptMsg(A128gcm, key, ciphertext, iv, tag);
+
+  char* recipientsSingle = "[{\"kid\":\"did:nuggets:sZziFvdXw8siMvg1P4YS91gG4Lc#key-p256-1\",\"kty\":\"EC\",\"crv\":\"P-256\",\"x\":\"A4NKTvWeEv3b-sJnlmwrATDklidT_qo3jTYRV2shaAc\",\"y\":\"_06GxhBcbxJzOCTz4F0kq_mETgGti33WkFpMKZHc-SY\"}]";
+  generalEncryptJson(EcdhEsA256kw, A128gcm, recipientsSingle, plaintext);
+
+  char* recipientsMultiple = "[{\"kid\":\"did:nuggets:sZziFvdXw8siMvg1P4YS91gG4Lc#key-p256-1\",\"kty\":\"EC\",\"crv\":\"P-256\",\"x\":\"A4NKTvWeEv3b-sJnlmwrATDklidT_qo3jTYRV2shaAc\",\"y\":\"_06GxhBcbxJzOCTz4F0kq_mETgGti33WkFpMKZHc-SY\"},{\"kid\":\"did:nuggets:qy8tyYBwveRXKDL2jjYTZENBDi3#key-p256-1\",\"kty\":\"EC\",\"crv\":\"P-256\",\"x\":\"YQbhZhp4ORKjwMqQIGFbIVSyYaaBuJbym_UWEWJPgbM\",\"y\":\"hxHEiOwPXUt1Nv_3MO5oRkUoMtYFaWIzW0iiZMNTnFE\"}]";
+  generalEncryptJson(EcdhEsA256kw, A128gcm, recipientsMultiple, plaintext);
+
+  char* jwe1 = "{\"protected\":\"eyJhbGciOiJFQ0RILUVTK0EyNTZLVyIsImVuYyI6IkExMjhHQ00iLCJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLWVuY3J5cHRlZCtqc29uIn0\",\"recipients\":[{\"header\":{\"kid\":\"did:nuggets:sZziFvdXw8siMvg1P4YS91gG4Lc#key-p256-1\",\"epk\":{\"kty\":\"EC\",\"crv\":\"P-256\",\"x\":\"gOck1VTJKClbIBckxyWDcvjgH7Hjh8l8JtZyMF_pcUg\",\"y\":\"erdiGdGNx_Bq3ZjIP0O6HqwnZ-hV4Qla1143vHg2CtA\"}},\"encrypted_key\":\"mYei_90yBUye5t54StnQWyZmpzgoaQ9N\"}],\"iv\":\"r7Tgd038slI_oE7v\",\"ciphertext\":\"0CPqmVTEOU3r\",\"tag\":\"WHzSP6R0tUK-w4UD1twngQ\"}";
+  char* jwk1 = "{\"kid\":\"did:nuggets:sZziFvdXw8siMvg1P4YS91gG4Lc#key-p256-1\",\"kty\":\"EC\",\"crv\":\"P-256\",\"d\":\"qjx4ib5Ea94YnyypBBPnvtGUuoRgGtF_0BtPuOSMJPc\"}";
+  decryptJson(jwe1, jwk1);
+
+  char* jwe2 = "{\"protected\":\"eyJhbGciOiJFQ0RILUVTK0EyNTZLVyIsImVuYyI6IkExMjhHQ00iLCJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLWVuY3J5cHRlZCtqc29uIn0\",\"recipients\":[{\"header\":{\"kid\":\"did:nuggets:sZziFvdXw8siMvg1P4YS91gG4Lc#key-p256-1\",\"epk\":{\"kty\":\"EC\",\"crv\":\"P-256\",\"x\":\"KEt3oBE9bpsu3meaYQvRmF_y6zNtml4ziN3fXq4Tpa8\",\"y\":\"qtoKMfk5bxo4_TEGz1GCJSwanNtt-enZvuWUi_42Pko\"}},\"encrypted_key\":\"8H45Ib-7SB8hFPBF7adVCt0fm1su4WZ_\"},{\"header\":{\"kid\":\"did:nuggets:qy8tyYBwveRXKDL2jjYTZENBDi3#key-p256-1\",\"epk\":{\"kty\":\"EC\",\"crv\":\"P-256\",\"x\":\"7Xdm9ui6DV3yT10oUe3kl-NAisnywvVTFp0TVo9ILVg\",\"y\":\"HcC88ngI0gHAKp7GR-a4E_VDEgqpnKs-yfgt0Lx-Lgw\"}},\"encrypted_key\":\"nPbYDVJPcL1w1aZsikU8uXwCwppGlvC4\"}],\"iv\":\"8flfc6gEcmROAsIq\",\"ciphertext\":\"jZ5hTy5pxSb0\",\"tag\":\"pfGFGYwsOcke0QrrT04Izw\"}";
+  char* jwk2 = "{\"kid\":\"did:nuggets:qy8tyYBwveRXKDL2jjYTZENBDi3#key-p256-1\",\"kty\":\"EC\",\"crv\":\"P-256\",\"d\":\"pndx4RjZSMpYjkokcn5xcIfmhZV19-jr_0n4l1kcphI\"}";
+  decryptJson(jwe2, jwk2);
 }
