43c8ca622f1f2f5acd7d650692b3fb9d403c8f1c
Andrew Lord <andrew.lord@appliedblockchain.com>
2022-09-09 15:58:14 +0100

    test: 'generalSignJson' for 'alg' value per jwk

--- __tests__/generalSign.jose.spec.ts
+++ __tests__/generalSign.jose.spec.ts
@@ -8,24 +8,51 @@ describe("jose", () => {
 
     describe("should sign payload correctly", () => {
 
-      describe("where alg type is `ECDSA`", () => {
+      it("with a single key", async () => {
+        const alg = 'ES256';
 
-        it("and `alg` = 'Es256'", async () => {
-          const alg: SigningAlgorithm = SigningAlgorithm.Es256;
+        // @ts-ignore
+        const signer_jwks: [JWK] = [ {
+          ...jwks[0].private,
+          alg
+        } ];
 
-          // @ts-ignore
-          const signer_jwks: [JWK] = [ jwks[0].private, jwks[1].private ];
+        const signed = await generalSignJson(payload, signer_jwks);
 
-          const signed = await generalSignJson(alg, payload, signer_jwks);
+        expect(signed.signatures.length).toBe(1);
+        expect(signed.signatures[0].header.kid).toBe('did:nuggets:sZziFvdXw8siMvg1P4YS91gG4Lc#key-p256-1');
+        expect(signed.signatures[0].protected).toBe('eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLXNpZ25lZCtqc29uIiwiYWxnIjoiRVMyNTYifQ');
+        expect(signed.payload).toBe('eyJoZWxsbyI6InRoZXJlIn0');
+      });
 
-          expect(signed.signatures.length).toBe(2);
-          expect(signed.signatures[0].header.kid).toBe('did:nuggets:sZziFvdXw8siMvg1P4YS91gG4Lc#key-p256-1');
-          expect(signed.signatures[0].protected).toBe('eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLXNpZ25lZCtqc29uIiwiYWxnIjoiRVMyNTYifQ');
-          expect(signed.signatures[1].header.kid).toBe('did:nuggets:qy8tyYBwveRXKDL2jjYTZENBDi3#key-p256-1');
-          expect(signed.signatures[1].protected).toBe('eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLXNpZ25lZCtqc29uIiwiYWxnIjoiRVMyNTYifQ');
-          expect(signed.payload).toBe('eyJoZWxsbyI6InRoZXJlIn0');
-        });
+      it("with multiple keys of the same `alg` type", async () => {
+        const alg = 'ES256';
 
+        // @ts-ignore
+        const signer_jwks: [JWK] = [ { ...jwks[0].private,  alg }, { ...jwks[1].private, alg } ];
+
+        const signed = await generalSignJson(payload, signer_jwks);
+
+        expect(signed.signatures.length).toBe(2);
+        expect(signed.signatures[0].header.kid).toBe('did:nuggets:sZziFvdXw8siMvg1P4YS91gG4Lc#key-p256-1');
+        expect(signed.signatures[0].protected).toBe('eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLXNpZ25lZCtqc29uIiwiYWxnIjoiRVMyNTYifQ');
+        expect(signed.signatures[1].header.kid).toBe('did:nuggets:qy8tyYBwveRXKDL2jjYTZENBDi3#key-p256-1');
+        expect(signed.signatures[1].protected).toBe('eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLXNpZ25lZCtqc29uIiwiYWxnIjoiRVMyNTYifQ');
+        expect(signed.payload).toBe('eyJoZWxsbyI6InRoZXJlIn0');
+      });
+
+      it("with multiple keys of different `alg` types", async () => {
+        // @ts-ignore
+        const signer_jwks: [JWK] = [ { ...jwks[0].private,  alg: 'ES256' }, { ...jwks[2].private, alg: 'ES512' } ];
+
+        const signed = await generalSignJson(payload, signer_jwks);
+
+        expect(signed.signatures.length).toBe(2);
+        expect(signed.signatures[0].header.kid).toBe('did:nuggets:sZziFvdXw8siMvg1P4YS91gG4Lc#key-p256-1');
+        expect(signed.signatures[0].protected).toBe('eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLXNpZ25lZCtqc29uIiwiYWxnIjoiRVMyNTYifQ');
+        expect(signed.signatures[1].header.kid).toBe('did:nuggets:qy8tyYBwveRXKDL2jjYTZENBDi3#key-p512-1');
+        expect(signed.signatures[1].protected).toBe('eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLXNpZ25lZCtqc29uIiwiYWxnIjoiRVM1MTIifQ');
+        expect(signed.payload).toBe('eyJoZWxsbyI6InRoZXJlIn0');
       });
 
     });
