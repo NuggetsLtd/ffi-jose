1d52fd6b0d49180724e5495dfb68b487cb0e02d7
Andrew Lord <andrew.lord@appliedblockchain.com>
2022-11-18 16:45:20 +0000

    test: update typescript FFI 'JWT' token type option

--- __tests__/flattenedSign.jose.spec.ts
+++ __tests__/flattenedSign.jose.spec.ts
@@ -10,18 +10,35 @@ describe("jose", () => {
 
       describe("where alg type is `ECDSA`", () => {
 
-        it("and `alg` = 'Es256'", async () => {
-          const alg: SigningAlgorithm = SigningAlgorithm.Es256;
+        describe("and `alg` = 'Es256'", () => {
 
-          // @ts-ignore
-          const jwk: JWK = jwks[0].private;
+          it("and didcomm = true", async () => {
+            const alg: SigningAlgorithm = SigningAlgorithm.Es256;
+  
+            // @ts-ignore
+            const jwk: JWK = jwks[0].private;
+  
+            const signed = await flattenedSignJson(alg, payload, jwk, true);
+  
+            expect(signed.protected).toBe('eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLXNpZ25lZCtqc29uIiwiYWxnIjoiRVMyNTYifQ');
+            expect(signed.header.kid).toBe('did:nuggets:sZziFvdXw8siMvg1P4YS91gG4Lc#key-p256-1');
+            expect(signed.payload).toBe('eyJoZWxsbyI6InRoZXJlIn0');
+          });
 
-          const signed = await flattenedSignJson(alg, payload, jwk);
+          it("and didcomm = false (default)", async () => {
+            const alg: SigningAlgorithm = SigningAlgorithm.Es256;
+  
+            // @ts-ignore
+            const jwk: JWK = jwks[0].private;
+  
+            const signed = await flattenedSignJson(alg, payload, jwk);
+  
+            expect(signed.protected).toBe('eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9');
+            expect(signed.header.kid).toBe('did:nuggets:sZziFvdXw8siMvg1P4YS91gG4Lc#key-p256-1');
+            expect(signed.payload).toBe('eyJoZWxsbyI6InRoZXJlIn0');
+          });
 
-          expect(signed.protected).toBe('eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLXNpZ25lZCtqc29uIiwiYWxnIjoiRVMyNTYifQ');
-          expect(signed.header.kid).toBe('did:nuggets:sZziFvdXw8siMvg1P4YS91gG4Lc#key-p256-1');
-          expect(signed.payload).toBe('eyJoZWxsbyI6InRoZXJlIn0');
-        });
+        })
 
       });
 
